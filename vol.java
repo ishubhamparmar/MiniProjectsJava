d5d118d6c80c399a257e5982831bb26b5310e26b
Shubham Parmar <76205430+ishubhamparmar@users.noreply.github.com>
2023-03-16 13:10:20 +0530

    Update vol.java

--- vol.java
+++ vol.java
@@ -1,3 +1,9 @@
+//Set of Boxes.
+/*
+   Insert the box details into the set. Create a box class and its attributes are length breadth and height (Double Type). Get the box details from
+   the user and insert into the set. We need to store the details of the boxes with different volumes when the volume of the box is the same as the
+   volume of previous boxes included don't add this box in the set.The boxes are called same when their volumes are equal.
+*/
 import java.util.*;
 class vol{
     
